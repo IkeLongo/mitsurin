14fee4e10ed9a33d9e00fe30a514e64d1762b7df
Isaac Longoria <isaac.longoria9136@gmail.com>
2025-10-06 16:38:56 -0500

    Added Discovery section

--- containers/home-page/discover-section/index.tsx
+++ containers/home-page/discover-section/index.tsx
@@ -29,10 +29,20 @@ export default function DiscoverSection() {
             — Dr. Michael Selva
           </cite>
 
-          {/* Button */}
-          <button className="bg-white hover:bg-gray-200 text-red-900 font-semibold px-8 py-4 rounded-lg text-lg transition-colors duration-300 shadow-lg hover:shadow-xl cursor-pointer">
-            Explore Our Products
-          </button>
+          <div className="flex flex-col items-center justify-center gap-6">
+            {/* Button */}
+            <button className="bg-white hover:bg-gray-200 text-red-900 font-semibold px-6 py-3 rounded-lg text-md lg:text-lg transition-colors duration-300 shadow-lg hover:shadow-xl cursor-pointer">
+              Explore Our Products
+            </button>
+
+            <button className="relative inline-flex h-12 overflow-hidden rounded-lg p-[1px] focus:outline-none focus:ring-2 focus:ring-red-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+              <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#B8860B_0%,#8B0000_50%,#B8860B_100%)]" />
+              <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg bg-red-900 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+                Explore Our Products
+              </span>
+            </button>
+          </div>
+          
         </div>
       </div>       
     </section>
