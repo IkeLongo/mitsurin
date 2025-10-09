5cc9572f5c2b41d7ba675d0130d5af374bf56638
Isaac Longoria <isaac.longoria9136@gmail.com>
2025-10-09 11:36:24 -0500

    Added safe area to explosion background

--- components/ui/background/background-beams-with-collision.tsx
+++ components/ui/background/background-beams-with-collision.tsx
@@ -74,6 +74,9 @@ export const BackgroundBeamsWithCollision = ({
         // h-screen if you want bigger
         className
       )}
+      style={{
+        paddingBottom: "env(safe-area-inset-bottom)",
+      }}
     >
       {beams.map((beam) => (
         <CollisionMechanism
@@ -91,6 +94,7 @@ export const BackgroundBeamsWithCollision = ({
         style={{
           boxShadow:
             "0 0 24px rgba(34, 42, 53, 0.06), 0 1px 1px rgba(0, 0, 0, 0.05), 0 0 0 1px rgba(34, 42, 53, 0.04), 0 0 4px rgba(34, 42, 53, 0.08), 0 16px 68px rgba(47, 48, 55, 0.05), 0 1px 0 rgba(255, 255, 255, 0.1) inset",
+          marginBottom: "env(safe-area-inset-bottom)",
         }}
       ></div>
     </div>
