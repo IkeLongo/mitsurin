358514d1da33cb274231a83ac7a1ab1177b67c0d
Isaac Longoria <isaac.longoria9136@gmail.com>
2025-10-09 10:57:03 -0500

    Adjustments top theme fonts --oswald

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,16 +2,23 @@ import MainNavbar from "@/components/ui/navigation/MainNavbar";
 import type { Metadata } from "next";
 import { Inter, JetBrains_Mono } from "next/font/google";
 import "./globals.css";
+import { link } from "framer-motion/m";
 
-const inter = Inter({
-  variable: "--font-inter",
-  subsets: ["latin"],
-});
+// const inter = Inter({
+//   variable: "--font-inter",
+//   subsets: ["latin"],
+// });
 
-const jetbrainsMono = JetBrains_Mono({
-  variable: "--font-jetbrains-mono",
-  subsets: ["latin"],
-});
+// const jetbrainsMono = JetBrains_Mono({
+//   variable: "--font-jetbrains-mono",
+//   subsets: ["latin"],
+// });
+
+// const oswald = Oswald({
+//   variable: "--font-oswald",
+//   subsets: ["latin"],
+//   weight: ["400", "500", "600", "700"], // Add weights you need
+// });
 
 export const metadata: Metadata = {
   title: "Mitsurin Wagyu Beef",
@@ -25,8 +32,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap" rel="stylesheet" />
+      </head>
       <body
-        className={`${inter.variable} ${jetbrainsMono.variable} antialiased`}
+        className={`antialiased`}
       >
         <MainNavbar />
         {children}
--- containers/construction-page/hero-section/index.tsx
+++ containers/construction-page/hero-section/index.tsx
@@ -5,13 +5,14 @@ export function HeroSection() {
   return (
     <BackgroundBeamsWithCollision>
       <div className="text-center relative z-20">
-        <h2 className="text-xl md:text-3xl lg:text-3xl font-bold text-black dark:text-white tracking-tight mb-4" style={{ fontFamily: 'Oswald, sans-serif' }}>          <div className="mb-2">OUR NEW WEBSITE</div>
+        <h2 className="text-xl md:text-3xl lg:text-3xl font-bold text-black dark:text-white tracking-tight mb-4 font-[oswald]">
+          <div className="mb-2">OUR NEW WEBSITE</div>
           <div className="relative mx-auto inline-block w-max [filter:drop-shadow(0px_1px_3px_rgba(27,_37,_80,_0.14))]">
             <div className="absolute left-0 top-[1px] bg-clip-text bg-no-repeat text-transparent bg-gradient-to-r py-4 from-red-800 via-orange-600 to-yellow-600 [text-shadow:0_0_rgba(0,0,0,0.1)]">
-              <span className="text-8xl md:text-9xl lg:text-9xl">COMING<br/>SOON</span>
+              <span className="text-8xl md:text-9xl lg:text-9xl font-[oswald]">COMING<br/>SOON</span>
             </div>
             <div className="relative bg-clip-text text-transparent bg-no-repeat bg-gradient-to-r from-red-800 via-orange-600 to-yellow-600 py-4">
-              <span className="text-8xl md:text-9xl lg:text-9xl">COMING<br/>SOON</span>
+              <span className="text-8xl md:text-9xl lg:text-9xl font-[oswald]">COMING<br/>SOON</span>
             </div>
           </div>
         </h2>
