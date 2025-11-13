5ebc98c5f61ee73207497071025e666f9d8992a1
Isaac Longoria <isaac.longoria9136@gmail.com>
2025-11-13 16:32:28 -0600

    Adjustments to Heading section on Why Wagyu page.

--- containers/why-wagyu-page/heading-section/index.tsx
+++ containers/why-wagyu-page/heading-section/index.tsx
@@ -26,10 +26,7 @@ export default function HeadingSection() {
 
         {/* Description */}
         <p className="mt-6 text-stone-100 text-lg max-w-3xl">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor 
-          incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
-          exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute 
-          irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+          Because you deserve beef that doesn't compromise on taste, texture, or integrity.
         </p>
       </section>
     </div>
