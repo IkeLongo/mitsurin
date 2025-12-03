5c119c37a0159b9ef18cb522f732c292e4fc9954
Isaac Longoria <isaac.longoria9136@gmail.com>
2025-12-03 15:02:01 -0600

    Added types for whole/half cow purchases

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -6,7 +6,8 @@ import {postType} from './postType'
 import {authorType} from './authorType'
 import {availabilityType} from './availabilityType'
 import {premiumCutsType} from './premiumCutsType'
+import {cowPurchaseType} from './cowPurchaseType'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [blockContentType, categoryType, postType, authorType, availabilityType, premiumCutsType],
+  types: [blockContentType, categoryType, postType, authorType, availabilityType, premiumCutsType, cowPurchaseType],
 }
