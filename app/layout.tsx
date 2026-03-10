480c65bbbe515b6439e875c443bf00203b74a755
Isaac Longoria <isaac.longoria9136@gmail.com>
2026-03-10 13:49:40 -0500

    Updated layout.tsx in root

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import Script from "next/script";
 import { ToastContainer } from 'react-toastify';
 import { AnalyticsProvider } from "@/app/components/analytics/analytics-provider";
+import AnalyticsGA4 from "./components/analytics/analytics-ga4";
+import ClarityScript from "./components/analytics/microsoft-clarity";
 
 import "./globals.css";
 import "@mux/mux-player";
@@ -16,8 +18,6 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
-
-const GA_ID = process.env.NEXT_PUBLIC_GA_ID;
 
 export default async function RootLayout({
   children,
@@ -37,32 +37,16 @@ export default async function RootLayout({
         <link rel="dns-prefetch" href="https://stream.mux.com" />
         <link rel="preconnect" href="https://stream.mux.com" crossOrigin="anonymous" />
         <link rel="preload" as="video" href="https://stream.mux.com/cB7VJ1hTqPrBTmnpDTRV2hMbUom4aPaqPHJXIhIcTps.m3u8" />
-        {GA_ID ? (
-          <>
-            <Script
-              src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
-              strategy="afterInteractive"
-            />
-            <Script id="ga-init" strategy="afterInteractive">
-              {`
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){window.dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', '${GA_ID}', {
-                  anonymize_ip: true
-                });
-              `}
-            </Script>
-          </>
-        ) : null}
+        <ClarityScript />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased overflow-x-hidden`}
       >
+        <ToastContainer />
+        <AnalyticsGA4 />
         <AnalyticsProvider>
           {children}
         </AnalyticsProvider>
-        <ToastContainer />
       </body>
     </html>
   );
